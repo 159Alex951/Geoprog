4fd23264230093097317c218321f001ccb76f040
159Alex951 <162427178+159Alex951@users.noreply.github.com>
2024-04-08 16:33:42 +0200

    1

---
+++ vererbung.py
@@ -0,0 +1,11 @@
+
+class tier:
+    def __init__(self, name, legs)
+        self.name = name
+        self.legs = legs
+
+    def essen(self):
+        print("mjam")
+
+    def bellen(self):
+        print("")
